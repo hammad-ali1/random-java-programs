11094aade5cb9d5706e892eb97d3f57a261b893f
Hammad Ali <hammad.a22002@gmail.com>
2021-09-25 15:12:46 +0500

    ver 1

--- graphics/Spiral/Spiral.java
+++ graphics/Spiral/Spiral.java
@@ -1,11 +1,10 @@
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.util.Random;
 
 public class Spiral extends JPanel{
     public void paintComponent(Graphics g){
-        Color[] colors = {Color.BLACK, Color.BLUE, Color.CYAN, Color.GRAY, Color.MAGENTA,
-            Color.PINK, Color.RED};     //array of colors that to paint the rainbow with
         super.paintComponent(g);
         int width = getWidth();
         int height = getHeight();
@@ -14,21 +13,27 @@ public class Spiral extends JPanel{
         int pointX = INITIAL_X;
         int pointY = INITIAL_Y;
         int diameter = (height  - INITIAL_Y) < (width - INITIAL_X )? (height  - INITIAL_Y): (width - INITIAL_X);  //diameter of the first semicircle
-        int rotation = 180; 
+        int rotation = 0; 
         final int GAP = 40;  //difference of diameter between two consecutive semi-circle
         int cntr = 0;       //counts number of semicircles drawn
         while(diameter > 0){ 
-            for(Color c : colors){ //loop through array of colors
-                cntr++;
+            Random random = new Random();
+            int R, G, B;
+            Color c;
+            while(Math.abs(rotation) <= 180){    
+                int incrementAngle = 30;
+                rotation = 0;
+                R = random.nextInt(255); G = random.nextInt(255); B = random.nextInt(255);
+                c = new Color(R, G, B);
                 g.setColor(c);
-                g.fillArc(pointX, pointY, diameter, diameter, 0, rotation); //draws semi-circle
-                pointY += GAP/2;    //setting y co-ordinate for the next semi-circle
-                pointX += cntr%2 == 0 ? 0 : GAP;   //setting x-co-ordinate of next semi-circle
-                diameter -= GAP;    //decrements diameter of next semicircle
-                rotation *= -1;     //setting rotation of next semi-circle in clockwise direction
-                if(diameter < 0)
-                    break;          //stop looping through colors if there is no semicircle to draw
+                g.fillArc(pointX, pointY, diameter, diameter, rotation, incrementAngle); //draws semi-circle
+                rotation += incrementAngle;
             }
+            cntr++;
+            pointY += GAP/2;    //setting y co-ordinate for the next semi-circle
+            pointX += cntr%2 == 0 ? 0 : GAP;   //setting x-co-ordinate of next semi-circle
+            diameter -= GAP;    //decrements diameter of next semicircle
+            rotation *= -1;     //setting rotation of next semi-circle in clockwise direction
         }
     }    
 }
